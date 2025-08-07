b8f4669235e03e2ec37d1ff829d2d56a4bb481e0
emirkuloglu <emirkuloglu16@gmail.com>
2025-08-07 18:26:58 +0300

    commit

--- Program.cs
+++ Program.cs
@@ -7,3 +7,26 @@ bool? isActive = null;
 Console.WriteLine(maas.HasValue);
 Console.WriteLine(maas.GetValueOrDefault());
 Console.WriteLine(isActive.GetValueOrDefault());
+
+
+//String Types
+
+// string? deki soru işareti, bu değişkenin null olabileceğini belirtir.
+// string? = var olarak da tanımlanabilir. ?ne gerek yoktur.
+
+Console.Write("adı: ");
+string? ad = Console.ReadLine();
+
+Console.Write("soyadı: ");
+var soyad = Console.ReadLine();
+
+Console.Write("yaşı: ");
+string? yas = Console.ReadLine();
+
+//String concat
+// string mesaj = ad + " " + soyad + " isimli kişi " + yas + " yaşındadır.";
+
+// String interpolation
+string mesaj = $"{ad} {soyad} isimli kişi {yas} yaşındadır.";
+
+Console.WriteLine(mesaj);
